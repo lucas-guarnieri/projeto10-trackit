ec3be5be8a714628de9afc7cb8c949dd3c76f6e7
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-04-03 19:44:24 -0300

    feat - display historic page message

--- src/components/Historic.js
+++ src/components/Historic.js
@@ -1,11 +1,34 @@
-import Top from "./Top"
-import Menu from "./Menu";
+    import Top from "./Top"
+    import Menu from "./Menu";
+    import styled from 'styled-components'
 
-export default function Historic(){
-    return(
-        <>
-            <Top />
-            <Menu />
-        </>
-    );
-}
+    export default function Historic(){
+        return(
+            <>
+                <Top />
+                <Container>
+                    <h1>Histórico</h1>
+                    <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+                
+                </Container>
+                <Menu />
+            </>
+        );
+    }
+
+    const Container = styled.div`
+        margin: 98px 17px 70px 17px;
+
+        h1 {
+            font-size: 23px;
+            line-height: 29px;
+            margin-bottom: 17px;
+            color: #126BA5;
+        }
+
+        p {
+            font-size: 18px;
+            line-height: 22px;
+            color: #666666;
+        }
+    `
